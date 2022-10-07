2960f8703b45e72330fc83ae90b3f2752b4b7459
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-10-07 16:28:46 -0400

    Update ESKO.sol

--- src/contracts/ESKO.sol
+++ src/contracts/ESKO.sol
@@ -4,15 +4,15 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /**
- * @title ESKO contract
+ * @title GO_KEKCHAIN contract
  * NOTE: Contract inherit from openzeppelin ERC20
  */
-contract ESKO is ERC20 {
+contract GO_KEKCHAIN is ERC20 {
     /**
      * @dev Each token has 18 decimals
      * Mint initial 75000 amount of tokens for the owner
      */
-    constructor() ERC20("ESKO", "ESKO") {
-        _mint(msg.sender, 75000 * 10**18);
+    constructor() ERC20("GO-KEKCHAIN", "gKEK") {
+        _mint(msg.sender, 100000 * 10**18);
     }
 }
