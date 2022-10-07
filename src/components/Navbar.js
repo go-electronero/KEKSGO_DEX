9776b86c16c0bbd00f6381a839f6953551f897cb
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-10-07 17:44:33 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ class Navbar extends Component{
               <li className="nav-item">
                 <a
                  className="nav-link small" 
-                 href={`https://etherscan.io/address/${this.props.account}`}
+                 href={`https://mainnet-explorer.kekchain.com/address/${this.props.account}`}
                  target="_blank"
                  rel="noopener noreferrer"
                  >
