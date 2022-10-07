256e4de2514dad67e9ab2704e20e812e57bca79c
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-10-07 17:56:07 -0400

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,8 @@
 export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000'
 export const GREEN = 'success'
 export const RED = 'danger'
+export const EXCHANGE_ADDRESS = "0xbD6D2681586719158a7491859424f2f4CA5EBf6B"
+export const TOKEN_ADDRESS = "0x27bCe644d1bdd3e297e01B480d32557123F5806A"
 
 export const DECIMALS = (10**18)
 
@@ -11,5 +13,6 @@ export const ether = (wei) => {
   }
 }
 
-// Tokens and ether have same decimal resolution
+// Tokens and ether have same decimal resolution... p.s. (NOT ALWAYS FREN!)
+// toDo fix call to token address(ca).decimals() 
 export const tokens = ether
