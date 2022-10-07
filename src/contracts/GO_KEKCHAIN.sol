e4e2ba12263b083054093dcfcc09feb94430607f
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-10-07 17:09:15 -0400

    Update and rename ESKO.sol to GO_KEKCHAIN.sol

--- src/contracts/GO_KEKCHAIN.sol
+++ src/contracts/GO_KEKCHAIN.sol
@@ -1,5 +1,11 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+
+abstract contract _MSG {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+}
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -13,6 +19,6 @@ contract GO_KEKCHAIN is ERC20 {
      * Mint initial 75000 amount of tokens for the owner
      */
     constructor() ERC20("GO-KEKCHAIN", "gKEK") {
-        _mint(msg.sender, 100000 * 10**18);
+        _mint(_msgSender(), 100000 * 10**18);
     }
 }
